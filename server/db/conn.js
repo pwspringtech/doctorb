a0ca2d8d87f8fd17b3e093e7d38a8cddf64303d0
William Griffin <w.griffin13@gmail.com>
2019-08-06 09:46:20 -0700

    initial commit

---
+++ server/db/conn.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const dbConnString = process.env.DATABASE_URL || 'postgres://localhost/toddberlandmd'
+
+const conn = new Sequelize(dbConnString, {
+  logging: false,
+});
+module.exports = conn;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    mode: 'development',
+    entry: ['babel-polyfill', './client/index.js'],
+    output: {
+      path: __dirname,
+      filename: './public/bundle.js'
+    },
+    context: __dirname,
+    devtool: 'source-maps',
+    module: {
+        rules: [
+        {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+        },
+        ],
+    },
+};
