5082a2e34010519175383168ec76df90f2c89d2d
Preston Chaplin <preston.chaplin@gmail.com>
2019-08-09 07:42:26 -0400

    resolving conflicts

--- client/App.js
+++ client/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import React, { Component, Fragment } from 'react';
 import Home from './Home';
 import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
-      <Container>
+      <Fragment>
         <Home />
-        <div>App Rendering</div>
         <Footer />
-      </Container>
+      </Fragment>
     );
   }
 }
