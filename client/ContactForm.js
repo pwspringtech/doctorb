4561e8bbba116fbc36ec392497dcb93125337d72
William Griffin <w.griffin13@gmail.com>
2019-08-15 16:13:28 -0700

    added validation and resolved the message upload process

--- client/ContactForm.js
+++ client/ContactForm.js
@@ -1,143 +1,216 @@
+/* eslint-disable complexity */
 /* eslint-disable default-case */
-import React, { Component, useState } from 'react';
-import Container from 'react-bootstrap/Container';
+import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import ContactFormSendPopup from './ContactFormSendPopup';
 
-class ContactForm extends Component {
+const ContactSchema = Yup.object().shape({
+        firstName: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+        lastName: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+        phone: Yup.string()
+            .min(10, 'Invalid phone number'),
+        email: Yup.string()
+            .email('Invalid email')
+            .required('Required'),
+        message: Yup.string()
+            .min(2, 'Too Short!')
+            .required('Required'),
+        terms: Yup.boolean()
+            .oneOf([true], 'Field must be checked')
+});
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            validated: false,
-        }
-    }
+export default function ContactForm() {
 
-    isInvalidFormInput = (type, value) => {
-        // type: email, phone number
-        switch (type) {
-            case 'email':
-                if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-                    return true
-                } else {
-                    return false
-                }
+    const [modalShow, setModalShow] = React.useState(false);
 
-        }
-    }
-
-    handleSubmit = event => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        this.setState({ validated: true })
-    }
-
-    handleChange = event => {
-        const tempState = { value: event.target.value }
-        this.setState({ lineitems: templineitems });
-    }
-
-    render() {
-        return (
-            <Card>
-                <Card.Body>
-                    <Card.Title>Contact Dr. Berland</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Send a message to Dr. Berland to find out more information or to book an appointment.</Card.Subtitle>
-                    <Row noGutters>
-                    <Col>
-                        <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
-                        <Form.Row className="mt-2">
-                            <Form.Group as={Col} md="6" controlId="validationCustom01">
-                            <Form.Label>First name</Form.Label>
-                            <Form.Control
-                                required
-                                type="text"
-                                placeholder="First name"
-                            />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                            <Form.Control.Feedback type="invalid">
-                                Please enter your first name.
-                            </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="6" controlId="validationCustom02">
-                            <Form.Label>Last name</Form.Label>
-                            <Form.Control
-                                required
-                                type="text"
-                                placeholder="Last name"
-                            />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                            <Form.Control.Feedback type="invalid">
-                                Please enter your last name.
-                            </Form.Control.Feedback>
-                            </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                        <Form.Group as={Col} md="12" controlId="validationCustomPhone">
-                            <Form.Label>Phone number</Form.Label>
-                            <InputGroup>
-                                <InputGroup.Prepend>
-                                <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
-                                </InputGroup.Prepend>
+    return (
+        <Card>
+            <ContactFormSendPopup
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+            <Card.Body>
+                <Card.Title>Contact Dr. Berland</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Send a message to Dr. Berland to find out more information or to book an appointment.</Card.Subtitle>
+                <Row noGutters>
+                <Col>
+                    <Formik
+                        initialValues={{
+                            message: undefined,
+                            firstName: undefined,
+                            lastName: undefined,
+                            email: undefined,
+                            phone: undefined,
+                            terms: false
+                        }}
+                        validationSchema={ContactSchema}
+                        onSubmit={(values, actions) => {
+                            // Send values to server API
+                            axios.post('/api/users/messages', values)
+                                .then(message => {
+                                    actions.setSubmitting(false);
+                                    actions.resetForm({
+                                        message: '',
+                                        firstName: '',
+                                        lastName: '',
+                                        email: '',
+                                        phone: '',
+                                        terms: false
+                                    });
+                                    setModalShow(true)
+                                })
+                                .catch(error => {
+                                    actions.setSubmitting(false);
+                                    actions.setErrors(error);
+                                    actions.setStatus({ msg: 'Could not send message' });
+                                })
+                        }}
+                        render={({
+                            values,
+                            errors,
+                            status,
+                            touched,
+                            handleBlur,
+                            handleChange,
+                            handleSubmit,
+                            isSubmitting,
+                        }) => (
+                            <Form noValidate onSubmit={handleSubmit}>
+                            <Form.Row className="mt-2">
+                                <Form.Group as={Col} md="6" controlId="validationFormik01">
+                                <Form.Label>First name</Form.Label>
                                 <Form.Control
-                                type="text"
-                                placeholder="Phone number"
-                                aria-describedby="inputGroupPrepend"
+                                    type="text"
+                                    name="firstName"
+                                    placeholder="First name"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.firstName}
+                                    isValid={touched.firstName && !errors.firstName}
+                                    isInvalid={touched.firstName && !!errors.firstName}
                                 />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                                 <Form.Control.Feedback type="invalid">
-                                    Please enter a valid phone number.
+                                    {errors.firstName}
                                 </Form.Control.Feedback>
-                            </InputGroup>
-                        </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                        <Form.Group as={Col} md="12" controlId="validationCustomEmail">
-                            <Form.Label>Email</Form.Label>
-                            <InputGroup>
-                                <InputGroup.Prepend>
-                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                                </InputGroup.Prepend>
+                                </Form.Group>
+                                <Form.Group as={Col} md="6" controlId="validationFormik02">
+                                <Form.Label>Last name</Form.Label>
                                 <Form.Control
-                                type="text"
-                                placeholder="Email"
-                                aria-describedby="inputGroupPrepend"
-                                required
+                                    type="text"
+                                    name="lastName"
+                                    placeholder="Last name"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.lastName}
+                                    isValid={touched.lastName && !errors.lastName}
+                                    isInvalid={touched.lastName && !!errors.lastName}
                                 />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                                 <Form.Control.Feedback type="invalid">
-                                    Please enter a valid email address.
+                                    {errors.lastName}
                                 </Form.Control.Feedback>
-                            </InputGroup>
-                        </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                            <Form.Group as={Col} md="12" controlId="exampleForm.ControlTextarea1">
-                                <Form.Label>Message</Form.Label>
-                                <Form.Control as="textarea" required rows="5" />
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                            <Form.Group as={Col} md="12" controlId="validationCustomPhone">
+                                <Form.Label>Phone number</Form.Label>
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                    <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Form.Control
+                                        type="text"
+                                        name="phone"
+                                        placeholder="Phone number"
+                                        aria-describedby="inputGroupPrepend"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.phone}
+                                        isValid={touched.phone && !errors.phone}
+                                        isInvalid={touched.phone && !!errors.phone}
+                                    />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.phone}
+                                </Form.Control.Feedback>
+                                </InputGroup>
                             </Form.Group>
-                        </Form.Row>
-                        <Form.Group>
-                            <Form.Check
-                            required
-                            label="Agree to terms and conditions"
-                            feedback="You must agree before submitting."
-                            />
-                        </Form.Group>
-                            <Button type="submit">Send</Button>
-                        </Form>
-                    </Col>
-                    </Row>
-                </Card.Body>
-            </Card>
-        );
-    }
+                            </Form.Row>
+                            <Form.Row>
+                            <Form.Group as={Col} md="12" controlId="validationCustomEmail">
+                                <Form.Label>Email</Form.Label>
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Form.Control
+                                        type="text"
+                                        name="email"
+                                        placeholder="Email"
+                                        aria-describedby="inputGroupPrepend"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        isValid={touched.email && !errors.email}
+                                        isInvalid={touched.email && !!errors.email}
+                                    />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.email}
+                                </Form.Control.Feedback>
+                                </InputGroup>
+                            </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} md="12" controlId="controlTextarea1">
+                                    <Form.Label>Message</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        required rows="5"
+                                        name="message"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.message}
+                                        isValid={touched.message && !errors.message}
+                                        isInvalid={touched.message && !!errors.message}
+                                    />
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.message}
+                                </Form.Control.Feedback>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Group>
+                                <Form.Check
+                                    name="terms"
+                                    label="Agree to terms and conditions"
+                                    feedback={errors.terms}
+                                    onChange={handleChange}
+                                    isInvalid={touched.terms && !!errors.terms}
+                                />
+                            </Form.Group>
+                                <Button type="submit" disabled={isSubmitting}>Send</Button>
+                            </Form>
+                        )}
+                    />
+                </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    );
 }
-
-export default ContactForm;
