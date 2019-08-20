20878c8211defa35890d6b6495b03e5444ff6bd7
William Griffin <w.griffin13@gmail.com>
2019-08-20 13:57:08 -0700

    changed host variable

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const { sync } = require('./db/sync');
 
 const PORT = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 
 sync().then(app.listen(PORT, host, () => {
     console.log(`Server is listening on port ${PORT} on host ${host}`)
