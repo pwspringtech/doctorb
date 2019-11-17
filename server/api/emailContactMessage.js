8574dd8b00ac9e3851b3ecccd4b89d3fd81c9624
William Griffin <w.griffin13@gmail.com>
2019-11-17 12:40:54 -0800

    changed contact form email setup

--- server/api/emailContactMessage.js
+++ server/api/emailContactMessage.js
@@ -2,26 +2,27 @@ const nodemailer = require('nodemailer');
 
 const emailContactMessage = (messageObj) => {
 
+    const privateKey = process.env.GSUITE_PRIVATE_KEY.replace(new RegExp('\\\\n', '\g'), '\n')
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
             type: 'OAuth2',
-            user: 'william@alabconsulting.com',
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.CLIENT_REFRESH_TOKEN
+            user: 'info@appagetech.com',
+            serviceClient: process.env.GSUITE_CLIENT_ID,
+            privateKey: privateKey,
         }
     });
 
     const mailResponse = transporter.sendMail({
-        from: 'william@alabconsulting.com',
-        to: 'toddberland@gmail.com',
-        bcc: 'preston.chaplin@gmail.com, w.griffin13@gmail.com',
+        from: 'info@appagetech.com',
+        to: 'william@appagetech.com',
+        //bcc: 'preston.chaplin@gmail.com, w.griffin13@gmail.com',
         subject: messageObj.subject,
         html: messageObj.html
-    });
+    })
+        .catch(error => console.error(error))
     return mailResponse
 
 }
--- server/db/sync.js
+++ server/db/sync.js
@@ -1,16 +1,8 @@
 const conn = require('./conn');
-const { Media } = require('./Models');
 
 const sync = () => {
     return conn.sync({ force: true })
 }
-
-// const syncAndSeed = () => {
-//     return sync()
-//         .then(() => {
-
-//         })
-// }
 
 module.exports = {
     sync
